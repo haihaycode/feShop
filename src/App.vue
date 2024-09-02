289fd3c15b3253331a32a893823c6884014fd01b
haihaycode <kienthuc177@gmail.com>
2024-09-02 22:04:40 +0700

    f6

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
+  <topNavigation></topNavigation>
   <bottomNavigation :navIndex="getNavigationBottom" :mdHidden="getMdHidden" />
-  <router-view class="mx-2 md:mx-28" />
-  <div style="height: 100px;"></div>
+
+  <router-view class="mx-2 md:mx-28 mt-20" />
+  <div style="height: 1000px;"></div>
 </template>
 
 
 <script>
 
 import bottomNavigation from './components/bottomNavigation.vue'
+import topNavigation from './components/topNavigation.vue'
 import { mapGetters } from 'vuex';
 export default {
   name: 'AppComponent',
   components: {
-    bottomNavigation
+    bottomNavigation,
+    topNavigation
   },
   computed: {
     ...mapGetters(['getNavigationBottom']),
---
+++ src/components/topNavigation.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <nav
+            class="fixed top-0 z-50 w-full flex justify-start md:justify-between gap-2 md:gap-0 px-2 md:px-20 py-5 items-center bg-white shadow-md">
+            <h1 class="text-xl text-gray-800 font-bold">Gizmo Shop</h1>
+            <div class="flex items-center">
+                <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 pt-0.5 text-gray-600" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                    <input class="ml-2 outline-none bg-transparent font-" type="text" name="search" id="search"
+                        placeholder="Search..." />
+                </div>
+                <ul class="flex items-center space-x-6">
+                    <li class="font-semibold text-gray-700 hidden md:block">Home</li>
+                    <li class="font-semibold text-gray-700 hidden md:block">Supply</li>
+                    <li class="hidden md:block">
+                        <!-- tài khoản -->
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <!-- tài khoản -->
+                    </li>
+                    <li class="hidden md:block">
+                        <!-- thông báo  -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
+                        </svg>
+                        <!-- thông báo  -->
+                    </li>
+                    <li class="hidden md:block">
+                        <!-- giỏ hàng  -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                        </svg>
+                        <!-- giỏ hàng  -->
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    name: "topNavigation"
+}
+</script>
