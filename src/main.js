01bb47995eb8fe526e97ba005335a2ecf5278204
haihaycode <kienthuc177@gmail.com>
2024-09-04 20:19:11 +0700

    f11

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import './assets/css/base/backgrounds.css'
 import './assets/tailwind.css'
+
 const app = createApp(App);
 
 app.use(store)
