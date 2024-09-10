af6c523e41e680cf6375135771b5f348fa97fbb6
haihaycode <kienthuc177@gmail.com>
2024-09-10 19:45:27 +0700

    f27

--- README.md
+++ README.md
@@ -6,30 +6,20 @@
 https://stackblitz.com/~/github.com/haihaycode/feShop
 ```
 
-## Project setup
+## cài đặt dự án
 
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+    <h2>Cài đặt dự án</h2>
+    <pre><code>npm install</code></pre>
 
-```
-npm run serve
-```
+    <h3>Biên dịch và tải lại tự động cho môi trường phát triển</h3>
+    <pre><code>npm run serve</code></pre>
 
-### Compiles and minifies for production
+    <h3>Biên dịch và nén cho môi trường sản xuất</h3>
+    <pre><code>npm run build</code></pre>
 
-```
-npm run build
-```
-
-### Lints and fixes files
-
-```
-npm run lint
-```
-
-### Customize configuration
-
-See [Configuration Reference](https://cli.vuejs.org/config/).
+    <h3>Kiểm tra và sửa lỗi cú pháp</h3>
+    <pre><code>npm run lint</code></pre>
--- src/components/Voucher.vue
+++ src/components/Voucher.vue
@@ -1,30 +1,12 @@
 <template>
-<div class="flex items-center justify-center space-x-9">
-  <div class="voucher">
-    <div class="voucher-content">
-        <h3>Mã Giảm Giá</h3>
-        <p>Giảm 20% cho đơn hàng từ 500k</p>
+    <div class="flex items-center justify-center space-x-1">
+        <div class="voucher">
+            <div class="voucher-content">
+                <h3>Mã Giảm Giá</h3>
+                <p>Giảm 20% cho đơn hàng từ 500k</p>
+            </div>
+        </div>
     </div>
-</div>
-<div class="voucher">
-    <div class="voucher-content">
-        <h3>Mã Giảm Giá</h3>
-        <p>Giảm 20% cho đơn hàng từ 500k</p>
-    </div>
-</div>
-<div class="voucher">
-    <div class="voucher-content">
-        <h3>Mã Giảm Giá</h3>
-        <p>Giảm 20% cho đơn hàng từ 500k</p>
-    </div>
-</div>
-<div class="voucher">
-    <div class="voucher-content">
-        <h3>Mã Giảm Giá</h3>
-        <p>Giảm 20% cho đơn hàng từ 500k</p>
-    </div>
-</div>
-</div>
 
 </template>
 
@@ -35,6 +17,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
