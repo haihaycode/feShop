448cfd8cfd8508dfb6c22f1b50ba2f3a3260c765
thansex000 <loclop3@gmail.com>
2024-09-29 20:51:18 +0700

    api address

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Address></Address>
   </div>
 </template>
+
+<script>
+import Address from "@/components/AdderessComponent.vue"
+export default {
+ components :{
+  Address
+ }
+}
+
+</script>
